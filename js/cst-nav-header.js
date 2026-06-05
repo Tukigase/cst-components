7f6df84da8306da8d8796ca2f1215cc43dd14d85
Taiga Kobayashi <ringatukigase@gmail.com>
2026-06-05 23:29:24 +0900

    Header Web Component Complete

--- js/cst-nav-header.js
+++ js/cst-nav-header.js
@@ -14,17 +14,38 @@ export class CstNavHeader extends HTMLElement {
     // 実際の描画ロジック
     render() {
         // 親のHTMLから title="..." で渡された値を受け取る（なければデフォルト値）
-        const titleText = this.getAttribute('title') || 'デフォルトタイトル';
+        const titleText = this.getAttribute('title') || 'MySites';
+        const toolName = this.getAttribute('toolName') || 'Tools';
 
         // 自分が置かれている場所を基準に、CSSの絶対URLを自動計算する
         const cssUrl = new URL('../css/cst-nav-header.css', import.meta.url).href;
+
+        // パスの取得
+        const path = window.location.pathname.toLowerCase();
+
+        const isTopPage = path.endsWith('/') || path.endsWith('/index.html');
+
+        let titleHtml = "";
+
+        if (isTopPage) {
+            titleHtml = titleText;
+        } else {
+            titleHtml = `${toolName} - <a href="./" class="header-inline-brand-link" title="トップページへ戻る">${titleText}</a>`
+        }
 
         // Shadow DOMの中に、CSSのリンクとHTML構造を流し込む
         this.shadowRoot.innerHTML = `
             <link rel="stylesheet" href="${cssUrl}">
             
             <header>
-                <h1>${titleText}</h1>
+                <h1>${titleHtml}</h1>
+                <a href="google.com" class="contact-icon" title="お問い合わせ" >
+                    <svg viewBox="0 0 24 24" width="22" height="22" stroke="currentColor" stroke-width="2" fill="none"
+                        stroke-linecap="round" stroke-linejoin="round">
+                        <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path>
+                        <polyline points="22,6 12,13 2,6"></polyline>
+                    </svg>
+                </a>
             </header>
         `;
     }
